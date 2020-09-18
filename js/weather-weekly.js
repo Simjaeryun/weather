63816fef20fcf3b7ce4eb2047e78654c92f6c07d
Simjaeryun <61261788+Simjaeryun@users.noreply.github.com>
2020-09-18 14:44:30 +0900

    20200918
    20200918

--- js/weather-weekly.js
+++ js/weather-weekly.js
@@ -1,13 +1,29 @@
-const weatherWeekly = document.querySelectorAll(".weather_weekly_day");
+const weatherWeeklyDay = document.querySelectorAll(".weather_weekly_day");
 
 function getWeek() {
   const date = new Date();
   const week = new Array("월", "화", "수", "목", "금", "토", "일");
-  weatherWeekly[0].innerHTML = `${week[date.getDay() - 1]}요일`;
-  weatherWeekly[1].innerHTML = `${week[date.getDay()]}요일`;
-  weatherWeekly[2].innerHTML = `${week[date.getDay() + 1]}요일`;
-  weatherWeekly[3].innerHTML = `${week[date.getDay() + 2]}요일`;
-  weatherWeekly[4].innerHTML = `${week[date.getDay() + 3]}요일`;
+  weatherWeeklyDay[0].innerHTML = `${
+    week[date.getDay() - 1] == undefined
+      ? `${week[7]}`
+      : week[date.getDay() - 1]
+  }요일`;
+  weatherWeeklyDay[1].innerHTML = `${week[date.getDay()]}요일`;
+  weatherWeeklyDay[2].innerHTML = `${
+    week[date.getDay() + 1] == undefined
+      ? `${week[date.getDay() - 6]}`
+      : week[date.getDay() + 1]
+  }요일`;
+  weatherWeeklyDay[3].innerHTML = `${
+    week[date.getDay() + 2] == undefined
+      ? `${week[date.getDay() - 5]}`
+      : week[date.getDay() + 2]
+  }요일`;
+  weatherWeeklyDay[4].innerHTML = `${
+    week[date.getDay() + 3] == undefined
+      ? `${week[date.getDay() - 4]}`
+      : week[date.getDay() + 3]
+  }요일`;
 }
 
 function init() {
@@ -15,3 +31,18 @@ function init() {
 }
 
 init();
+
+/*
+      $(function () {
+        const date = new Date();
+        const week = new Array("월", "화", "수", "목", "금", "토", "일");
+        $(".weather_weekly_day:first-child").html(week[date.getDay() - 1]);
+        $(".weather_weekly_day:nth-child(2)").html(week[date.getDay()]);
+        $(".weather_weekly_day:nth-child(3)").html(week[date.getDay() + 1]);
+        $(".weather_weekly_day:nth-child(4)").html(week[date.getDay() + 2]);
+        $(".weather_weekly_day:last-child").html(week[date.getDay() + 3]);
+        if ($(".weather_weekly_day").html != week[date.getDay()]) {
+          console.log(this);
+        }
+      });
+      */
--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,10 @@
+const API_KEY = "bfd963a4ab3bab3b8e923c431315e3ca";
+
+fetch(
+  `https://api.openweathermap.org/data/2.5/forecast?id=1835841&appid=${API_KEY}`,
+  `http://api.openweathermap.org/data/2.5/forecast?id=${API_KEY}&lang=kr`
+);
+
 const weatherDate = document.querySelector(".weather>.weather_date");
 
 function getTime() {
@@ -7,9 +14,12 @@ function getTime() {
   const day = date.getDate();
   const hours = date.getHours();
   const minutes = date.getMinutes();
-  weatherDate.innerHTML = ` ${year}년 ${
-    month + 1
-  }월 ${day}일 ${hours}시 ${minutes}분`;
+  const seconds = date.getSeconds();
+  weatherDate.innerHTML = ` ${year}년 ${month + 1}월 ${day}일 ${
+    hours < 10 ? `0${hours}` : hours
+  }시 ${minutes < 10 ? `0${minutes}` : minutes}분 ${
+    seconds < 10 ? `0${seconds}` : seconds
+  }초`;
 }
 
 function init() {
